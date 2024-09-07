5342b74b4eed8a5a2df1f2dedcadf38e41a75098
KamalAborida <kamal.o.aborida@gmail.com>
2024-09-07 06:44:20 +0300

    homepage mobile tablet added

--- src/app/(shared)/NavBar/NavBar.tsx
+++ src/app/(shared)/NavBar/NavBar.tsx
@@ -1,12 +1,34 @@
+'use client';
+
 import Image from 'next/image';
 import NavList from './NavList';
 import NavIconList from './IconsList';
+import { useState } from 'react';
 
 export default function NavBar() {
+  const [isHamburger, setIsHamburger] = useState(false);
+
   return (
     <nav className="nav">
-      <Image src={'/logo.svg'} alt="Audio Store Logo" width={143} height={25} />
-      <NavList />
+      <div className="nav__hamLogo">
+        {isHamburger && (
+          <Image
+            src={'/hamburger-icon.svg'}
+            alt="Hamburger"
+            width={15}
+            height={15}
+            className="nav__hamLogo__hamburger"
+          />
+        )}
+        <Image
+          src={'/logo.svg'}
+          alt="Audio Store Logo"
+          width={143}
+          height={25}
+          className="nav__hamLogo__logo"
+        />
+      </div>
+      {!isHamburger && <NavList />}
       <NavIconList />
     </nav>
   );
