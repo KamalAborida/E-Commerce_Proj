98d6928bc4d2537e0e18bf2688c61168893cac8b
KamalAborida <kamal.o.aborida@gmail.com>
2024-10-06 03:03:19 +0300

    Modified Product and Category to be standAlone

--- src/app/(shared)/Categories/Category.tsx
+++ src/app/(shared)/Categories/Category.tsx
@@ -12,23 +12,23 @@ export default function Category({ href, name }: categoryProps) {
   const title = nameArr.join('');
 
   return (
-    <div className="categories__category">
+    <div className="category">
       <Image
         src={`/${name}-noBackground.svg`}
         alt="HeadPhones"
         width={120}
         height={160}
-        className="categories__category__img"
+        className="category__img"
       />
       <Image
         src={'/shadow.svg'}
         alt="Shadow"
         width={120}
         height={20}
-        className="categories__category__shadow"
+        className="category__shadow"
       />
-      <h2 className="categories__category__name">{title}</h2>
-      <Link href={`/${href}`} className="categories__category__shop">
+      <h2 className="category__name">{title}</h2>
+      <Link href={`/${href}`} className="category__shop">
         <p>Shop</p>
         <Image src={'/arrow-right.svg'} alt="arrow" width={5} height={10} />
       </Link>
