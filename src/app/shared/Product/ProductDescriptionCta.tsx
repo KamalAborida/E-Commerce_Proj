5b249bdc2640fab67d37b99ca2eefa97e2cafb1b
KamalAborida <kamal.o.aborida@gmail.com>
2024-11-04 21:09:22 +0200

    modified some product classes styles

--- src/app/shared/Product/ProductDescriptionCta.tsx
+++ src/app/shared/Product/ProductDescriptionCta.tsx
@@ -10,14 +10,16 @@ export default function ProductDescriptionCta({
   description,
 }: ProductDescriptionCtaProps) {
   return (
-    <div className="productCta__productDescriptionCta">
+    <div className="productCta__productDescriptionCta productDescriptionCta">
       {isNew > 0 && (
-        <span className="productCta__productDescriptionCta__isNew">
+        <span className="productCta__productDescriptionCta__isNew productDescriptionCta__isNew">
           NEW PRODUCT
         </span>
       )}
-      <h1 className="productCta__productDescriptionCta__name">{name}</h1>
-      <p className="productCta__productDescriptionCta__description">
+      <h1 className="productCta__productDescriptionCta__name productDescriptionCta__name">
+        {name}
+      </h1>
+      <p className="productCta__productDescriptionCta__description productDescriptionCta__description">
         {description}
       </p>
     </div>
