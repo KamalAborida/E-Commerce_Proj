e874c6d6cb15a36142d5e1a13f32bdee9b2a4055
KamalAborida <kamal.o.aborida@gmail.com>
2024-11-05 20:58:38 +0200

    Changed Database Creation Path

--- src/app/server/db.ts
+++ src/app/server/db.ts
@@ -3,11 +3,13 @@ import Database from 'better-sqlite3';
 import path from 'path';
 
 // Define the path for the database file
-const dbPath = path.join(__dirname, 'audio_store.db');
+const dbPath = path.join(process.cwd(), 'data\\audio_store.db');
 
-// const db = new Database(dbPath);
-const db = new Database(
-  'D:\\Web projects\\E-Commerce_Proj\\app\\data\\audio_store.db'
-);
+console.log(dbPath);
+
+const db = new Database(dbPath);
+// const db = new Database(
+//   'D:\\Web projects\\E-Commerce_Proj\\app\\data\\audio_store.db'
+// );
 
 export default db;
